a8e1713723a3bc7ea54aaff639f9c20eafa4e31c
StepanMaksimov <stepan.maximov@mytona.com>
2024-10-23 16:43:40 +0700

    radio

---
+++ Assets/_Project/Scripts/car/interactables/Radio.cs
@@ -0,0 +1,26 @@
+﻿using _Project.Scripts.interaction;
+using UnityEngine;
+
+namespace _Project.Scripts.car.interactables
+{
+    public class Radio: MonoBehaviour, IInteractable
+    {
+        [SerializeField] private string itemName = "radio";
+        [SerializeField] private string itemHint = "press E to switch radio";
+        
+        public void Interact()
+        {
+            Debug.Log("switching radio channel...");
+        }
+
+        public string GetName()
+        {
+            return itemName;
+        }
+
+        public string GetHint()
+        {
+            return itemHint;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/radio_station/RadioStation.cs
@@ -0,0 +1,9 @@
+﻿using _Project.Scripts.utils;
+
+namespace _Project.Scripts.radio_station
+{
+    public class RadioStation: Singleton<RadioStation>
+    {
+        
+    }
+}
